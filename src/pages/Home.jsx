4370b668feca0e6a00d893d49e5fb2e3d71db477
AndreaC05 <aguirreandrea457@gmail.com>
2025-09-17 10:36:18 -0500

    avance

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,32 @@
+import Contenido from "../components/Contenido";
+import "../style/Home.css";
+
 export default function Home() {
-    return(
-        <>
-        
-        </>
-    );
+  return (
+    <>
+      <div className="container_home">
+        <video
+          className="video-background"
+          src="https://res.cloudinary.com/dnao6nouz/video/upload/v1758121036/PinDown.io__sixthells_1758121011_wyvcft.mp4"
+          autoPlay
+          muted
+          loop
+          playsInline
+        ></video>
+
+        {/* Video principal normal */}
+        <video
+          className="video-main"
+          src="https://res.cloudinary.com/dnao6nouz/video/upload/v1758121036/PinDown.io__sixthells_1758121011_wyvcft.mp4"
+          autoPlay
+          muted
+          loop
+          playsInline
+        ></video>
+        <div className="contenido_text">
+          <Contenido />
+        </div>
+      </div>
+    </>
+  );
 }
